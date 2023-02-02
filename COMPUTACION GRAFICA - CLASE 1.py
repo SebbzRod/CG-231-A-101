f866545cb0179722cbb3d216df226fea44ecd54b
SebbzRod <124273952+SebbzRod@users.noreply.github.com>
2023-02-02 09:14:47 -0500

    Add files via upload

---
+++ COMPUTACION GRAFICA - CLASE 1.py
@@ -0,0 +1,30 @@
+
+"""
+COMPUTACION GRAFICA () - JUAN SEBASTIÁN RODRÍGUEZ RODRÍGUEZ 
+"""
+
+    # METODO SORT
+
+Numeros = []
+
+num1 = input("Por favor digite el primer numero: ")1
+num2 = input("Por favor digite el segundo numero: ")
+num3 = input("Por favor digite el tercer numero: ")
+num4 = input("Por favor digite el cuarto numero: ")
+
+Numeros.append(num1)
+Numeros.append(num2)
+Numeros.append(num3)
+Numeros.append(num4)
+
+print(Numeros) #Lista desordenada
+
+Numeros.sort()
+print(Numeros) #Lista ordenada menor a mayor
+
+Numeros.sort(reverse = True)
+print(Numeros) #Lista ordenada menor a mayor
+
+
+
+    
